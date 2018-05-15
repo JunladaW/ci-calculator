ff35a221d785e77a0a56a3b32ac6342013f6f10e
junlada <ubungkee@gmail.com>
2018-05-15 11:33:24 +0700

    Test change code and build

--- app/src/androidTest/java/com/example/junlada/calculator/CalculatorPage.java
+++ app/src/androidTest/java/com/example/junlada/calculator/CalculatorPage.java
@@ -3,9 +3,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 //Example Page Object Pattern
 public class CalculatorPage {
-    public static void inputNumber1 (String input){
+    public static void inputNumber (String input){
         onView(withId(Integer.parseInt(input)))
                 .perform(replaceText(input)
                         , closeSoftKeyboard());
